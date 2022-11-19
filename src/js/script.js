99d1b4faa1d25189012f77a421e1a515532319a5
Delmer Ballesteros <delmerjohnb@gmail.com>
2022-11-19 20:45:30 +0800

    change svg to img

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ tl.from(".main-bottom-text", {
   x: 500,
 }).from(".nav-items", {
   opacity: 0,
-  duration: 0.5,
+  duration: 2,
   y: -50,
   stagger: 0.2,
 });
@@ -14,26 +14,28 @@ gsap.to(".title-anim", {
   duration: 2,
   opacity: 1,
   stagger: 0.5,
+  delay: 1,
 });
 
 gsap.to(".main-bottom-text", {
   "clip-path": "polygon(100% 0%, 100% 100%, 0% 100%, 0% 0%)",
   duration: 2,
   opacity: 1,
+  delay: 1,
 });
 
 gsap.to(".about-container", {
   "clip-path": "polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)",
   duration: 2,
   opacity: 1,
+  delay: 1,
 });
 
 gsap.from("#project", {
   scrollTrigger: {
     trigger: "#project",
-    start: "top center",
+    start: "center bottom",
   },
   x: 200,
   opacity: 0,
-  duration: 2,
 });
