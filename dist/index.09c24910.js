00673edff155c2ddf523258a8c0aad80ad303f39
Delmer Ballesteros <delmerjohnb@gmail.com>
2022-12-11 20:11:31 +0800

    refactor some code

--- dist/index.09c24910.js
+++ dist/index.09c24910.js
@@ -13,23 +13,13 @@ gsap.to(".title-anim", {
     delay: 1,
     y: 0
 });
-gsap.to(".main-bottom-text", {
-    "clip-path": "polygon(100% 0%, 100% 100%, 0% 100%, 0% 0%)",
-    duration: 2,
-    opacity: 0.5,
-    delay: 1
-});
 gsap.to(".about-container", {
     "clip-path": "polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)",
     duration: 2,
     opacity: 1,
     delay: 1
 });
-gsap.to(".card", {
-    scrollTrigger: {
-        trigger: "#about",
-        start: "center center"
-    },
+gsap.to(".project-card", {
     opacity: 1,
     duration: 1.5,
     stagger: 0.4,
@@ -37,10 +27,6 @@ gsap.to(".card", {
     y: 0
 });
 gsap.to(".card-title", {
-    scrollTrigger: {
-        trigger: "#about",
-        start: "center center"
-    },
     opacity: 1,
     duration: 1.5,
     stagger: 0.2,
